c8f270e5bca2afbd1f46e385324520e67f31d759
Giuliano <giulianobotelho@outlook.com.br>
2024-09-14 11:27:31 -0300

    first commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,11 @@
 
 import { Dialog} from './components/ui/dialog';
-
 import { CreateGoal } from './components/ui/create-goal';
-import { EmptyGoals } from './components/empty-goals';
 import { Summary } from './components/ui/Summary';
 const App: React.FC = () => {
   return (
     <Dialog>
-      {/* <EmptyGoals/> */}
+      
       <Summary/>
     <CreateGoal/>
     </Dialog>
